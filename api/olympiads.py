04658938e8a5604572cb52644d401ba51699e035
Vubni <eimanaeg@gmail.com>
2025-09-02 00:17:57 +0300

    open news global

--- api/olympiads.py
+++ api/olympiads.py
@@ -21,21 +21,10 @@ from datetime import date
         400: {"description": "Отсутствует один из параметров", "schema": sh.Error400Schema},
         401: {"description": "Авторизация не выполнена"},
         500: {"description": "Server-side error (Ошибка на стороне сервера)"}
-    },
-    parameters=[{
-        'in': 'header',
-        'name': 'Authorization',
-        'schema': {'type': 'string', 'format': 'Bearer'},
-        'required': True,
-        'description': 'Bearer-токен для аутентификации'
-    }]
+    }
 )
 async def get(request: web.Request) -> web.Response:
     try:
-        user_id = await core.check_authorization(request)
-        if not isinstance(user_id, int):
-            return user_id
-        
         res = await func.get()
         
         return web.json_response(res, status=200)
